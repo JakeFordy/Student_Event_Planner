eb9162bcbbffa9a6a77d20553e6a4d7983435b4e
hte202 <hte202@exeter.ac.uk>
2026-02-17 14:19:57 +0000

    Create Algorithm_Tests.py

---
+++ Algorithm_Tests.py
@@ -0,0 +1,20 @@
+'''
+This file is to implement the testing of the bruteforce and dynamic programming algorithms
+'''
+
+# Test the small imput size for both algorithms
+# Compare the output to a known (computed by hand) algorithm
+# This verifies that the algorithm 'work' 
+
+# Mention possibility of edge-cases 
+# i.e. test for file not found, isuse converting to int
+
+# Measure time complexity of algorithms
+# O notation and time measurements for different cases
+# Test this for small, medium, large
+
+# Also, test this for n = 10, 15, 20, 25, 30, ...
+# Print statement, and export to csv file 
+
+# Stress testing, use high number of itterations of random ints
+

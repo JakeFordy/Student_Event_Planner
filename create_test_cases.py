799f130e21f6bd13ee362f849035251a4d8c7e92
hte202 <hte202@exeter.ac.uk>
2026-02-20 00:02:16 +0000

    Benchmark testing, and input file creation

--- create_test_cases.py
+++ create_test_cases.py
@@ -1,26 +1,3 @@
-'''
-This file is to implement the testing of the bruteforce and dynamic programming algorithms
-Test the small imput size for both algorithms
-Compare the output to a known (computed by hand) algorithm
-This verifies that the algorithm 'work' 
-
-Mention possibility of edge-cases 
-i.e. test for file not found, isuse converting to int
-
-Measure time complexity of algorithms
-O notation and time measurements for different cases
-Test this for small, medium, large
-
-Also, test this for n = 10, 15, 20, 25, 30, ...
-Print statement, and export to csv file 
-
-Stress testing, use high number of itterations of random ints
-
-Both run as perform_algorithms()
-Or individually run as bruteforce_costonly(activity_num, cost_left, current_selection, activities)
-Or dynamic_costonly(total_budget, activities)
-'''
-
 import random
 from pathlib import Path
 
